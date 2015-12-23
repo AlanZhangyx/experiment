3e969ccb114f5c46f62abd097bb75b76a778672f
ZhangYaxu <alanzhangyx@163.com>
2015-12-23 22:07:18 +0800

    prepare solve follows conflict.

--- experiment/src/main/java/com/ddup/chatroom/RoomServer.java
+++ experiment/src/main/java/com/ddup/chatroom/RoomServer.java
@@ -8,12 +8,19 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class RoomServer {
-
+	
 	public static void main(String[] args) throws IOException {
 		InetAddress bindAddr = InetAddress.getLocalHost();
 		ServerSocket server = new ServerSocket(20161, 100, bindAddr);
-		Socket socket = server.accept();
-		OutputStream os = socket.getOutputStream();
+		
+		while(true){
+			Socket socket = server.accept();
+			
+			
+			
+		}
+		
+		
 		
 		/*byte[] buffer = new byte[1024];
 		InputStream input = new InputStream();
