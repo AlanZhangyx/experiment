bdabba906cf5c870fccb7eca5974e9af16531f86
ZhangYaxu <alanzhangyx@163.com>
2015-12-27 19:24:49 +0800

    TCP and one of it's implements about chatroom.

--- experiment/src/main/java/com/ddup/chatroom/RoomServer.java
+++ experiment/src/main/java/com/ddup/chatroom/RoomServer.java
@@ -1,40 +1,44 @@
 package com.ddup.chatroom;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class RoomServer {
-
-	public static void main(String[] args) throws IOException {
-		InetAddress bindAddr = InetAddress.getLocalHost();
-		ServerSocket server = new ServerSocket(20161, 100, bindAddr);
-
-		while (true) {
-			// 阻塞监听
-			Socket socket = server.accept();
-
-			// 读取
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-
-			// 存放进String
-			StringWriter sw = new StringWriter();
-			// 存放
-			String line = null;
-			while ((line = br.readLine()) != null) {
-
+	
+	private static int SERVER_PORT_INDEX = 10001;
+	
+	/**
+	 * 递增端口，获得一个可用的ServerSocket
+	 * @return
+	 */
+	private static synchronized ServerSocket getNewServer(){
+		
+		ServerSocket server = null;
+		while (server == null && SERVER_PORT_INDEX <20000) {
+			try {
+				server = new ServerSocket(SERVER_PORT_INDEX);
+			} catch (IOException e) {
+				System.out.println(SERVER_PORT_INDEX);
+				SERVER_PORT_INDEX ++;
 			}
-
 		}
-
-		/*
-		 * InputStream input = new InputStream(); os.write(b, off, len);
-		 */
-
+		return server;
 	}
 
+	/**
+	 * 
+	 * @param args
+	 * @throws IOException 
+	 * @throws UnknownHostException 
+	 */
+	public static void main(String[] args) throws IOException{
+		ServerSocket ss = new ServerSocket(Integer.valueOf(args[0]));
+		while (true) {
+			Socket socket = ss.accept();
+			new Thread(new NewSocketTread(socket)).start();
+		}
+	}
 }
---
+++ experiment/src/main/java/com/ddup/java/MainTest2.java
@@ -0,0 +1,11 @@
+package com.ddup.java;
+
+public class MainTest2 {
+
+	public static void main(String[] args) {
+		for (String string : args) {
+			System.out.println(string);
+		}
+	}
+
+}
