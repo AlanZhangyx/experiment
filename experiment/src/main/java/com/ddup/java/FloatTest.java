6d4aea5f4614e2abe4eedae5e703567c2b3edd12
dznzyx <zhangyaxu@iwhere.com>
2015-12-31 17:13:12 +0800

    对浮点数还是没有清楚

--- experiment/src/main/java/com/ddup/java/FloatTest.java
+++ experiment/src/main/java/com/ddup/java/FloatTest.java
@@ -2,10 +2,33 @@ package com.ddup.java;
 
 public class FloatTest {
 
+	/**
+	 * @Title
+	 * @Description
+	 * @param args
+	 * @TestUrl
+	 */
 	public static void main(String[] args) {
 		float f1 = .1f;
 		float f2 = .1f;
 		System.out.println(f1 * f2);
+		
+		float f3 = 31.1234567891f;
+		float f5 = 63.1234567891f;
+		float f6 = 64.1234567891f;
+		float f7 = -91.1234567891f;
+		float f11 = 179.666666f;
+		System.out.println(f3);
+		System.out.println(f5);
+		System.out.println(f6);
+		System.out.println(f7);
+		System.out.println(f11);
+		
+		Integer i1 = Integer.MAX_VALUE;
+		System.out.println(i1);
+		
+		float f4 = Integer.MAX_VALUE;
+		System.out.println(f4);
 	}
 
 }
