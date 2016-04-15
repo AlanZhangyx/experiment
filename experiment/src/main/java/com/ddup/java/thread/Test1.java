156d48029bfc46e7d4e70a50c2784985929cf9e5
alan zhang <alanzhangyx@163.com>
2016-04-15 18:12:49 +0800

    1. 容器和线程目录的建立，并没有实际内容

---
+++ experiment/src/main/java/com/ddup/java/thread/Test1.java
@@ -0,0 +1,24 @@
+package com.ddup.java.thread;
+
+/**
+ * 每个对象都有
+ * 	1. 一个关联的monitor
+ * 	2. 一个关联的线程wait set
+ * 
+ * 
+ * <br>
+ * <br>
+ * <br>
+ * <strong>Time</strong>:2016年4月15日<br>
+ *
+ * @version : 1.0.0
+ */
+public class Test1 {
+
+	public static void main(String[] args) {
+		//ThreadLocal
+		//ReentrantLock
+		//Thread
+	}
+
+}
