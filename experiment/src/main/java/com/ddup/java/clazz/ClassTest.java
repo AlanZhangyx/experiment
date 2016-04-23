76aa7f9552bcd7136ca51039c67519cd82168d4a
ZhangYaxu <alanzhangyx@163.com>
2016-04-24 01:23:33 +0800

    1. JDBCAPI中的java.sql包research。
    研究了：DriverManager、Connection
    Statement
    	PrepareStatement
    		CallableStatement

---
+++ experiment/src/main/java/com/ddup/java/clazz/ClassTest.java
@@ -0,0 +1,11 @@
+package com.ddup.java.clazz;
+
+public class ClassTest {
+
+	public static void main(String[] args) {
+		Class<ClassTest> clazz = ClassTest.class;
+	}
+	
+	public void _sdf(){}
+
+}
