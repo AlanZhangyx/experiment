789db875feba0f809689e5373b65431f8ffc5948
alanzhang <alanzhangyx@163.com>
2016-05-25 17:27:33 +0800

    ActiveMQ temp2

--- experiment/src/main/java/com/ddup/activemq/Producer.java
+++ experiment/src/main/java/com/ddup/activemq/Producer.java
@@ -1,6 +1,12 @@
 package com.ddup.activemq;
 
+import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
 
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
@@ -17,10 +23,60 @@ public class Producer {
 	
 	
 	public static void main(String[] args){
+		//ActiveMQ is an open sourced implementation of JMS 1.1 as part of the J2EE 1.4 specification.
+		
+		//two messaging styles 
+		//    point-to-point messaging using queues
+		//    publish-and-subscribe messaging using topics
+		
+		
+		//********************* JMS API
+		
+		//*** common APIs
+		//Message,BytesMessage,MapMessage,ObjectMessage,StreamMessage,TextMessage
+		//Queue for PTP; Topic for pub/sub
+		//Destination is supertype of Queue and Topic
+		
+		//*** classic API interfaces
+		//ConnectionFactory used by a client to create a Connection.
+		//Connection is an active connection to a JMS provider.
+		//Session is a single-threaded context for sending and receiving messages.
+		//MessageProducer created by a Session that is used for sending messages to a queue or topic.
+		//MessageConsumer created by a Session that is used for receiving messages sent to a queue or topic.
+		
+		//*** Simplified API interfaces   -- same classic but fewer interfaces
+		//ConnectionFactory ...create a JMSContext.
+		//JMSContext == Connection + Session
+		//JMSProducer same MessageConsumer but created by JMSContext
+		//JMSConsumer same MessageConsumer but created by JMSContext
+		
+		//client uses to create a connection with a JMS provider.
 		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
 				USERNAME,
 				PASSWORD,
 				BROKERURL
 				);
+		
+		Connection connection = null;
+		Session session = null;
+		Destination destination = null;
+		MessageProducer messageProducer = null;
+		MessageConsumer messageConsumer = null;
+		try {
+			connection = connectionFactory.createConnection();
+			connection.start();
+			
+			session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+			
+			destination = session.createQueue("daily_news");
+			
+			messageProducer = session.createProducer(destination);
+			
+//			messageProducer.send(message);
+			
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
